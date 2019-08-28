3d9c442287fcaae74a93c6961a301b34e9cf8b78
吴翰文 <p78o233@126.com>
2019-08-28 17:34:23 +0800

    20190828 sql数据

--- src/main/resources/sql/shares.sql
+++ src/main/resources/sql/shares.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2019-08-27 23:16:45
+Date: 2019-08-28 17:33:30
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,15 +21,17 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `buy_sell_record`;
 CREATE TABLE `buy_sell_record` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '所有投资收益记录表',
-  `buyPrice` float DEFAULT NULL,
+  `buyPrice` float DEFAULT NULL COMMENT '买入单价',
   `buyTime` date DEFAULT NULL,
-  `sellPrice` float DEFAULT NULL,
+  `sellPrice` float DEFAULT NULL COMMENT '卖出单价',
   `sellTime` date DEFAULT NULL,
   `profitOrLoss` float DEFAULT NULL COMMENT '盈亏，盈利正数亏损负数',
   `stockNum` varchar(10) DEFAULT NULL COMMENT '编号',
   `stockName` varchar(10) DEFAULT NULL COMMENT '名称',
   `category` int(11) DEFAULT NULL COMMENT '1 股票 2基金 3黄金 4期货 5彩票',
   `stockId` int(11) DEFAULT NULL COMMENT '彩票这个为0',
+  `isFinish` bit(11) DEFAULT b'0' COMMENT '是否已经完成了',
+  `buyNum` int(11) DEFAULT NULL COMMENT '买入数量',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -80,7 +82,7 @@ CREATE TABLE `stock_record` (
   `category` int(11) DEFAULT NULL COMMENT '1 股票 2基金 3黄金 4期货',
   `recordTime` datetime DEFAULT NULL COMMENT '记录创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of stock_record
@@ -95,3 +97,13 @@ INSERT INTO `stock_record` VALUES ('13', '1.59', '1.62', '1.62', '1.58', '7', 's
 INSERT INTO `stock_record` VALUES ('14', '7.93', '7.99', '8.03', '7.89', '8', 'sh600740', '山西焦化', '1', '2019-08-27 00:00:00');
 INSERT INTO `stock_record` VALUES ('15', '1.15', '1.18', '1.2', '1.14', '9', 'sh601558', 'ST锐电', '1', '2019-08-27 00:00:00');
 INSERT INTO `stock_record` VALUES ('16', '0.95', '0.95', '0.96', '0.94', '10', 'sh600240', '*ST华业', '1', '2019-08-27 00:00:00');
+INSERT INTO `stock_record` VALUES ('17', '1.5', '1.52', '1.54', '1.49', '1', 'sh600022', '山东钢铁', '1', '2019-08-28 00:00:00');
+INSERT INTO `stock_record` VALUES ('18', '2.76', '2.78', '2.82', '2.74', '2', 'sh600698', '*ST天雁', '1', '2019-08-28 00:00:00');
+INSERT INTO `stock_record` VALUES ('19', '6.26', '6.28', '6.4', '6.23', '3', 'sh600879', '航天电子', '1', '2019-08-28 00:00:00');
+INSERT INTO `stock_record` VALUES ('20', '4.55', '4.55', '4.59', '4.55', '4', 'sh600497', '驰宏锌锗', '1', '2019-08-28 00:00:00');
+INSERT INTO `stock_record` VALUES ('21', '0.9', '0.93', '0.96', '0.88', '5', 'sh600747', '*ST大控', '1', '2019-08-28 00:00:00');
+INSERT INTO `stock_record` VALUES ('22', '1.18', '1.15', '1.19', '1.15', '6', 'sh600614', '*ST鹏起', '1', '2019-08-28 00:00:00');
+INSERT INTO `stock_record` VALUES ('23', '1.62', '1.65', '1.68', '1.6', '7', 'sh603077', '和邦生物', '1', '2019-08-28 00:00:00');
+INSERT INTO `stock_record` VALUES ('24', '7.97', '8.05', '8.09', '7.88', '8', 'sh600740', '山西焦化', '1', '2019-08-28 00:00:00');
+INSERT INTO `stock_record` VALUES ('25', '1.17', '1.17', '1.18', '1.16', '9', 'sh601558', 'ST锐电', '1', '2019-08-28 00:00:00');
+INSERT INTO `stock_record` VALUES ('26', '0.94', '0.94', '0.95', '0.93', '10', 'sh600240', '*ST华业', '1', '2019-08-28 00:00:00');
